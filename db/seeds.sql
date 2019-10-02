36326116ab0b400bcf49aa4a28e0819df1c05437
Ryan Mobley <ryan.mobley94@gmail.com>
2019-10-02 15:59:24 -0500

    db

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-USE burgers_db;
-TRUNCATE burgers_db.burgers;
-INSERT INTO burgers_db.burgers (burger_name,author,eaten)
+USE or8h7n82yt69ob10;
+TRUNCATE or8h7n82yt69ob10.burgers;
+INSERT INTO or8h7n82yt69ob10.burgers (burger_name,author,eaten)
 VALUES ('Cheese_Burger','Ryan_Mobley',0),('Bacon_Cheese Burger','Lazy_Foo',0),('Double_Cheese_Burger',"Burge_Burgeson",0),
 ('Bacon_Bleu_Burger',"steve",0);
